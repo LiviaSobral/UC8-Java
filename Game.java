05f3d7529f58d2aa9885d04a4010bc68871ce886
LiviaSobral <134714074+LiviaSobral@users.noreply.github.com>
2025-02-21 15:00:17 -0300

    Add files via upload

--- Game.java
+++ Game.java
@@ -2,27 +2,50 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package guessingGame;
+package com.mycompany.rpsGame;
 import java.util.Scanner;
 /**
  *
- * @author Aluno
+ * @author LIVIAHELOISAALVESSOB
  */
-public class Game {
-    private int numero;   
+public class Game {  
+    private int vitoriasMaquina = 0;
+    private Player jogador;
+    private Validator valida;
+    private Scanner scan; 
     
     public Game(){
-        this.numero = (int)(Math.random()*100)+1;
+        this.jogador = new Player();
+        this.valida = new Validator();
+        this.scan = new Scanner(System.in);
     }
+       
+   
 
-    public void start() {
-        throw new UnsupportedOperationException("Ainda n faz nada");
-    }
-    public int getNumero() {
-        return numero;
-    }
-    public void setNumero(int numero) {
-        this.numero = numero;
-    }
+    void start() {
+        for(int jogadas = 0;jogadas < 3&&jogador.getVitorias() < 2 &&vitoriasMaquina < 2;jogadas++){
+            int escolha;
+            escolha = (int) (Math.random() * 3) + 1;
+            System.out.println("Escreva Sua Escolha Pedra(1), Papel(2), Tesoura(3)");
+            int escolhaPlayer = scan.nextInt();
+            int resp = valida.validar(escolha, escolhaPlayer);
+            if(resp==1){
+                jogador.setVitorias(jogador.getVitorias()+1);
+            }else if(resp==2){
+                vitoriasMaquina++;
+            }
+            
+        }
+        if(jogador.getVitorias() == vitoriasMaquina){
+                System.out.println("Player "+ jogador.getVitorias()+" Empatou com a maquina.");
+            }else{
+            System.out.println("Player "+ jogador.getNome()+" teve Vitorias: "+ jogador.getVitorias());
+            System.out.println(("A Maquina teve Vitorias: "+vitoriasMaquina));
+        }
+        
+            
+        
+        
+        }
     
 }
---
+++ PedraPapelTesoura.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.rpsGame;
+
+/**
+ *
+ * @author LIVIAHELOISAALVESSOB
+ */
+public class PedraPapelTesoura {
+
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.start();
+    }
+}
