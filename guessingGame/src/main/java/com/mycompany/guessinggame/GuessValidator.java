3cea3be3743b19b69d971b573b24765f3c30ad0d
LiviaSobral <134714074+LiviaSobral@users.noreply.github.com>
2025-05-13 16:11:09 -0300

    Add files via upload

---
+++ guessingGame/src/main/java/com/mycompany/guessinggame/GuessValidator.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.guessinggame;
+
+/**
+ *
+ * @author LIVIAHELOISAALVESSOB
+ */
+public class GuessValidator {
+    public boolean validateGuess(int guess, int numberToGuess) {
+        if (guess > numberToGuess) {
+            System.out.println("Seu palpite e muito alto!");
+            return false;
+        } else if (guess < numberToGuess) {
+            System.out.println("Seu palpite e muito baixo!");
+            return false;
+        } else {
+            return true; // Palpite correto
+        }
+    }
+}
